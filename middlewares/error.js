2c85c81bcb197a67e3249013ea24b49357a148b1
Prasanna Desai <xdeveloper.codex@gmail.com>
2022-07-06 08:55:45 +0530

    USer Model and Error Handling

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,11 +1,50 @@
 const ErrorHandler = require('../utils/errorhandler');
 
+const sendDevErrors = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendProdErrors = (err, res) => {
+  if (err.isOperationalError) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'internal server error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendDevErrors(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = err;
+    if (err.name === 'CastError') {
+      const message = `Invalid ${err.path}: ${err.value}`;
+      error = new ErrorHandler(message, 400);
+    }
+    if (err.code === 11000) {
+      const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+      const message = `Duplicate field value: ${value}. Please use another value!`;
+      error = new ErrorHandler(message, 400);
+    }
+    if (error.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map((el) => el.message);
+      const message = `Invalid input data. ${errors.join('. ')}`;
+      error = new ErrorHandler(message, 400);
+    }
+    sendProdErrors(error, res);
+  }
 };
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const mongoose = require('mongoose');
 
+//handle uncought Exception
+process.on('uncaughtException', (err) => {
+  console.log(`Error:${err.message}`);
+  console.log('Shutting down server because of uncaughtException');
+  process.exit(1);
+});
 const app = require('./app');
 
 const DB = process.env.DB_URI.replace(
@@ -23,4 +29,14 @@ mongoose
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+});
+
+//unhandler promise rejection
+process.on('unhandledRejection', (err) => {
+  console.log(`Error:${err.message}`);
+  console.log('Shutting down server because of unhandled promise rejection');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
